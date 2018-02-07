7ca71040f0213d007a205ce1578cc0f779e67b64
github username <github email address>
2018-02-07 15:13:39 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,12 @@ require 'pry'
 require_relative "../lib/student.rb"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+
+
+#  namespace :db do
+#    desc 'migrade changes to your database'
+#    task :migrate => environment do
+#      Student.create_table
+#    end
+# end
